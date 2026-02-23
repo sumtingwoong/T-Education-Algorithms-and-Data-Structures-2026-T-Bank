9355c3385eb5dbd9a915b87c40a06ddf1ed5b5ca
SumTingWong <43645220+sumtingwoong@users.noreply.github.com>
2026-02-23 18:29:32 +0100

    Task A solution added

--- contest 1/a.py
+++ contest 1/a.py
@@ -1,1 +1,18 @@
+nk = list(map(int, input().split()))
+elements = list(map(int, input().split()))
+questions = list(map(int, input().split()))
 
+for i in questions:
+    left = 0
+    right = nk[0] - 1
+    flag = False
+    while left <= right:
+        mid = (left + right) // 2
+        if elements[mid] < i:
+            left = mid + 1
+        elif elements[mid] > i:
+            right = mid - 1
+        else:
+            flag = True
+            break
+    print('YES') if flag else print('NO')
