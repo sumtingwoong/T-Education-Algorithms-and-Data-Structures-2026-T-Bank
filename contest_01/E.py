5b1f0903e243617a07f19972d1f26620f4c496af
SumTingWong <rsanik112885@gmail.com>
2026-02-26 19:11:54 +0100

    contest_01/E.py added. problem solved

--- contest_01/E.py
+++ contest_01/E.py
@@ -1,5 +1,20 @@
 # a*(x**3) + b*(x**2) + cx + d = 0
 
+def f(x):
+    return ((a*x + b)*x + c)*x + d
+
+
 a, b, c, d = map(int, input().split())
 
-print(a, b, c, d)
+l = -1e6
+r = 1e6
+
+for i in range(100):
+    cur = (l + r) / 2
+    if f(l) * f(cur) <= 0:
+        r = cur
+    else:
+        l = cur
+
+print(f'{(l + r) / 2:.4f}')
+
