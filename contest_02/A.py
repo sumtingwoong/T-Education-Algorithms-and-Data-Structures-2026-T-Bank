f08b8e480a7d48c0fa52c00b7e7f38b0f60dc4a8
Rustam Suleimenov <rsanik112885@gmail.com>
2026-03-18 18:50:08 +0100

    contest_02/A.py added, problem Solved

--- contest_02/A.py
+++ contest_02/A.py
@@ -1,1 +1,25 @@
-ьь
+import sys
+
+input = sys.stdin.buffer.readline
+n = int(input())
+stack = []
+ans = []
+
+for _ in range(n):
+    act = input().split()
+    if act[0] == b'1':
+        cur = int(act[1])
+        if not stack:
+            stack.append((cur, cur))
+        else:
+            cur_min = stack[-1][1]
+            if cur < cur_min:
+                stack.append((cur, cur))
+            else:
+                stack.append((cur, cur_min))
+    elif act[0] == b'2':
+        stack.pop()
+    else:
+        ans.append(str(stack[-1][1]))
+
+sys.stdout.write('\n'.join(ans))
